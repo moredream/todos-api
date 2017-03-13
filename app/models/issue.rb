baf903380a222b50366c4f9db95d6d6858b300d4
Hyunuk Jung <hujung@baikal.io>
2017-03-13 17:45:54 +0800

    parsing result hashs

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -1,5 +1,28 @@
+
 class Issue < ApplicationRecord
 
   validates_presence_of :issue_url, :user_name, :user_id, :body
+
+
+    # @number = params[:number]
+    # @owner = params[:owner]
+    # @repo = params[:repo]
+  def self.getIssue(id)
+	@client = GithubClient.new({owner: "plataformatec", repo:"devise", number: 2692})
+	comments = @client.comments
+
+	comments.each do |comment|
+		puts comment["issue_url"]
+		puts comment["user"]["login"]
+		puts comment["user"]["id"]
+		puts comment["body"]	
+	end
+  end
+
+  def getIssue(id)
+	@client = GithubClient.new({owner: "plataformatec", repo:"devise", number: 2692})
+	comments = @client.comments
+
+  end
 
 end
