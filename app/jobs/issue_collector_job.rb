7f0aa95ccfa7e554a87d8bb4331f23827ba5bf42
Hyunuk Jung <hujung@baikal.io>
2017-03-13 20:18:44 +0800

    add workers with sidekiq

--- app/jobs/issue_collector_job.rb
+++ app/jobs/issue_collector_job.rb
@@ -2,6 +2,6 @@ class IssueCollectorJob < ApplicationJob
   queue_as :default
 
   def perform(*args)
-    # Do something later
+    Issue.create(args)
   end
 end
--- app/models/issue.rb
+++ app/models/issue.rb
@@ -7,22 +7,44 @@ class Issue < ApplicationRecord
     # @number = params[:number]
     # @owner = params[:owner]
     # @repo = params[:repo]
-  def self.getIssue(id)
-	@client = GithubClient.new({owner: "plataformatec", repo:"devise", number: 2692})
-	comments = @client.comments
+	# params {owner: "plataformatec", repo:"devise", number: 2692}
+  def self.getIssue(params)
+		@client = GithubClient.new(params)
+		comments = @client.comments
 
-	comments.each do |comment|
-		puts comment["issue_url"]
-		puts comment["user"]["login"]
-		puts comment["user"]["id"]
-		puts comment["body"]	
+		comments.each do |comment|
+
+			issue_args   = { issue_url:  comment["issue_url"] ,
+											user_name: comment["user"]["login"],
+											user_id: comment["user"]["id"],
+											body:  comment["body"] }
+			Issue.create(issue_args)
+		end
 	end
-  end
 
-  def getIssue(id)
-	@client = GithubClient.new({owner: "plataformatec", repo:"devise", number: 2692})
-	comments = @client.comments
+	def self.getIssueLater(id)
+		@client = GithubClient.new({owner: "plataformatec", repo:"devise", number: 2692})
+		comments = @client.comments
 
-  end
+		comments.each do |comment|
+
+			issue_args   = { issue_url:  comment["issue_url"] ,
+											 user_name: comment["user"]["login"],
+											 user_id: comment["user"]["id"],
+											 body:  comment["body"] }
+			# Issue.create(issue_args)
+			IssueCollectorJob.perform_later issue_args
+
+		end
+
+		#IssueCollectorJob.perform_later args
+
+	end
+
+  # def getIssue(id)
+  # @client = GithubClient.new({owner: "plataformatec", repo:"devise", number: 2692})
+  # comments = @client.comments
+  #
+  # end
 
 end
